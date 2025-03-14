bd39c26153dc0f632732fccc15862f9df7d594aa
MahidulRana <mahidulislamrana003@gmail.com>
2025-03-14 23:38:53 +0100

    create sequence class and List of Sequence objects for CancerPrediction

--- source/Samples/ApprovedMultiSequenceLearningNew/CancerPrediction.cs
+++ source/Samples/ApprovedMultiSequenceLearningNew/CancerPrediction.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,28 @@ namespace ApprovedMultiSequenceLearningNew
 {
     internal class CancerPrediction
     {
+        public class Sequence
+        {
+            public string name { get; set; }
+            public char[] data { get; set; }
+        }
 
+        /// <summary>
+        /// Reads a JSON file containing a List of Sequence objects.
+        /// </summary>
+        private static List<Sequence> ReadDataset(string path)
+        {
+            Console.WriteLine("Reading Sequence...");
+            try
+            {
+                string fileContent = File.ReadAllText(path);
+                return JsonConvert.DeserializeObject<List<Sequence>>(fileContent);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to read the dataset: {ex.Message}");
+                return new List<Sequence>();
+            }
+        }
     }
 }
